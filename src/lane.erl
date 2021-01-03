c526a80d83b292672477c7778eeb5ff15fb4538a
MagicJesus <46258083+MagicJesus@users.noreply.github.com>
2021-01-03 19:17:42 +0100

    Final Working Version

--- src/lane.erl
+++ src/lane.erl
@@ -1,44 +1,51 @@
 -module(lane).
 
--compile(export_all).
+-export([
+	loop/0,
+	swimmer/3,
+	start/2,
+	config/2
+	]).
 
 start(ID, Capacity) ->
 	spawn(?MODULE, config, [ID, Capacity]).
-	
+
 
 config(ID, Capacity) ->
 	put(id, ID),
 	put(capacity, Capacity),
 	loop().
-	
+
 loop() ->
 	receive
 		{Sender, {request, available}} ->
 			Cap = get(capacity),
 			case Cap =:= 0 of
-				true -> 
+				true ->
 					Sender ! {self(), 0};
 				false ->
 					Sender ! {self(), {avialable, Cap}}
 			end;
 		{Sender, {request, state}} ->
-			Actual = get(capacity),
-			Ident = get(id),
-			Sender ! {self(), {status, Ident, Actual}};
+			Cap = get(capacity),
+			ID = get(id),
+			Sender ! {self(), {status, ID, Cap}};
 		{Sender, {swim, Time}} ->
 			spawn(?MODULE, swimmer, [self(), Time, get(id)]),
 			NewCap = get(capacity) - 1,
 			put(capacity, NewCap),
 			Sender ! {get(id), {swimmer_entered}};
 		{_ , {swimmer, finished}} ->
-			io:format("Swimmer finished swimming on lane ID: ~w\n", [get(id)]),
+			io:format("\n \nSwimmer finished swimming on lane ID: ~w\n \n", [get(id)]),
 			NewCap = get(capacity) + 1,
 			put(capacity, NewCap);
-		{Sender, _} ->
-			Sender ! io:format("ID: ~w Capacity: ~w\n", [get(id), get(capacity)])
+		{terminate} ->
+			exit(user_exit);
+		_ ->
+			c:flush(), []
 	end,
 	loop().
 
 swimmer(Parent, Time, ID) ->
-	io:format("Swimmer entering lane ~w\n", [ID]),
+	io:format("\nSwimmer entering lane ~w\n", [ID]),
 	timer:send_after(Time, Parent, {self(), {swimmer, finished}}).
--- src/pool.erl
+++ src/pool.erl
@@ -1,39 +1,96 @@
 -module(pool).
 -behaviour(gen_server).
 
--compile(export_all).
+-export([
+	menu/0,
+	swim/0,
+	swim/1,
+	status/0,
+	start/0,
+	start/1,
+	stop/0,
+	message/1,
+	lane_processes/1,
+	check_for_first_free_lane/1,
+	collect/1,
+	not_zero/1,
+	put_list/1,
+	get_input/0
+	]).
+-export([
+	init/1,
+	handle_cast/2,
+	handle_call/3,
+	handle_info/2,
+	terminate/2,
+	code_change/3
+	]).
 
 -define(DefaultSwimmingLanes, 4).
 -define(DefaultCapacity, 4).
 
+% -- funkcja do obslugiwania programu
+menu() ->
+	io:format("\n## WELCOME TO THE SIMPLEST POOL SIMULATOR ##\n"),
+	io:format("   What would you like to do? \n"),
+	io:format("1: Check the pool status \n"),
+	io:format("2: Enter the pool for desired amount of time (in seconds)\n"),
+	io:format("3: Exit \n"),
+	{ok, UserInput} = io:read("Your choice: "),
+	case UserInput of
+		1 ->
+			io:format("\n\n---------STATUS CHECK---------\n\n"),
+			status(),
+			io:format("\n\n"),
+			menu();
+		2 ->
+			{ok, Time} = io:read("Enter the amount of time: "),
+			swim(Time * 1000),
+			timer:sleep(100),
+			menu();
+		3 ->
+			stop();
+		_ ->
+			io:format("Undefined Command, please enter 1, 2 or 3\n\n"),
+			menu()
+	end.
 % -- funkcja do 'wchodzenia do basenu'
-swim(Time) when is_integer(Time) -> 
+swim(Time) when is_integer(Time) ->
 	gen_server:call(?MODULE, {swim, Time}); % gen_server:call wywołuje moja metode handle_call/3
 swim(_) ->
 	message({invalid_args, "swim()"}).
+swim() ->
+	message({no_args_given, "swim()", "1"}).
 % -- funkcja do sprawdzania aktualnego stanu torów
 status() ->
 	gen_server:call(?MODULE, {check_status}).
 
 % -- funkcja start() odpala serwer i tworzy odpowiednia ilosc torów
 start() ->
-	start({?DefaultSwimmingLanes, ?DefaultCapacity}).
+	io:format("Would you like to change the number of lanes and their capacity (default 4/4)? (y/n)\n"),
+	get_input().
 start({NumOfLanes, Capacity}) 	when is_integer(NumOfLanes)
 								andalso is_integer(Capacity) ->
 	Number = max(NumOfLanes, 1),
-	gen_server:start_link({local, ?MODULE}, ?MODULE, [Number, Capacity], []);
+	gen_server:start_link({local, ?MODULE}, ?MODULE, [Number, Capacity], []),
+	menu();
 start(_) ->
 	message({invalid_args, "start()"}).
+% -- funkcja do zatrzymania i zamknięcia basenu
+stop() ->
+	gen_server:stop(?MODULE).
 
 % -- init() jest wolany przy wywolaniu gen_server:start_link()
 init([SwimmingLanes, Capacity]) ->
     process_flag(trap_exit, true),
     {ok, #{ lane_pids => [ lane:start(ID, Capacity) || ID <- lists:seq(1, SwimmingLanes) ]}}.
 
-% -- pomocnicza funkcja do informowania o bledzie	
+% -- pomocnicza funkcja do informowania o bledach
 message({invalid_args, FunctionName}) ->
-    FunctionName ++ " > Invalid Argument(s).".
-	
+    FunctionName ++ " > Invalid Argument(s).";
+message({no_args_given, FunctionName, ArgNum}) ->
+	FunctionName ++ " must have at at least " ++ ArgNum ++ " argument(s)".
+
 % -- handle_call/3 to callback do gen_server:call
 handle_call({swim, Time}, _From, State) ->
 	PIDs = lane_processes(State),
@@ -50,28 +107,32 @@ handle_call({check_status}, _From, State) ->
 	PIDs = lane_processes(State),
 	[PID ! {self(), {request, state}} || PID <- PIDs],
 	Response = collect(PIDs),
-	{reply, {ok, Response}, State}.
-	
-handle_cast(_Request, State) -> 
+	put_list(Response),
+	{reply, ok, State}.
+
+handle_cast(_Request, State) ->
 	{noreply, State}.
 
-handle_info(_Request, State) -> 
+handle_info(_Request, State) ->
 	{noreply, State}.
 
-terminate(_Reason, _State) -> 
+terminate(_Reason, State) ->
+	io:format("User Exit \n"),
+	PIDs = lane_processes(State),
+	[PID ! {terminate} || PID <- PIDs],
 	ok.
 
-code_change(_OldVsn, State, _Extra) -> 
+code_change(_OldVsn, State, _Extra) ->
 	{ok, State}.
-
+% -- funkcja pomocnicza, pobiera PIDy procesów torów pływackich
 lane_processes(State) ->
     maps:get(lane_pids, State, []).
 
+% -- funkcja zwraca PID pierwszego wolnego toru
 check_for_first_free_lane(PIDs) ->
-	[PID ! {self(), {request, available}} || PID <- PIDs], % wyslanie prosby o informacje o dostepnosci toru	
+	[PID ! {self(), {request, available}} || PID <- PIDs], % wyslanie prosby o informacje o dostepnosci toru
 	Response = collect(PIDs),
 	Filtered = lists:filter(fun(X) -> not_zero(X) end, Response),
-	% ta funkcja bedzie zwracac id pierwszego wolnego napotkanego toru
 	case length(Filtered) =:= 0 of
         true -> 0;
         false ->
@@ -83,18 +144,40 @@ check_for_first_free_lane(PIDs) ->
 collect([]) -> [];
 collect(PIDs) ->
     receive
-		{PID, {status, Ident, Actual}} ->
-			[ {Ident, Actual} | collect(PIDs -- [PID])];
+		{PID, {status, ID, Cap}} ->
+			[ {ID, Cap} | collect(PIDs -- [PID])];
 		{PID, {avialable, _}} ->
-      [ PID | collect(PIDs -- [PID])];
-    {PID, Response } ->
-      [ Response | collect(PIDs -- [PID])];
-    _ -> c:flush(), [] % Fail quietly and flush message queue.
+      		[ PID | collect(PIDs -- [PID])];
+    	{PID, Response } ->
+      		[ Response | collect(PIDs -- [PID])];
+    	_ -> c:flush(), [] % Fail quietly and flush message queue.
     end.
+% -- funkcja pomocnicza do wyodrebniania PID-ów torów pływackich z listy
 not_zero(X) ->
 	case X =:= 0 of
 		true ->
 			false;
 		false ->
 			true
+	end.
+
+% funkcja pomocnicza do wypisywania danych o stanie torów
+put_list([]) ->
+	io:format("");
+put_list([{ID,Cap}|Tail]) ->
+	io:format("Current capacity on lane ~w is : ~w\n", [ID, Cap]),
+	put_list(Tail).
+% -- funkcja do pobierania danych od uzytkownika
+get_input() ->
+	{ok, UserInput} = io:read(""),
+	case UserInput of
+		y ->
+			{ok, UserLanes} = io:read("Enter the desired number of lanes: "),
+			{ok, UserCapacity} = io:read("Enter the desired lane capacity: "),
+			start({UserLanes, UserCapacity});
+		n ->
+			start({?DefaultSwimmingLanes, ?DefaultCapacity});
+		_ ->
+			io:format("Invalid input, please enter a single character (y or n)\n"),
+			get_input()
 	end.
